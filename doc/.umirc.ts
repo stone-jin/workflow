ffb75111952132e4df81451e03f99ea84149896d
stone-jin <hzjinbing@163.com>
2021-08-20 21:45:58 +0800

    chore: umi base

--- doc/.umirc.ts
+++ doc/.umirc.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'dumi';
 export default defineConfig({
   title: 'Site Name',
   mode: 'site',
+  base: '/workflow/',
   publicPath: process.env.NODE_ENV === 'production' ? '/workflow/' : '/',
   // more config: https://d.umijs.org/config
 });
