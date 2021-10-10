d0c166c6e7b000ce49bf53cf75af9cec8b286462
stone-jin <hzjinbing@163.com>
2021-10-11 01:33:01 +0800

    chore: test

--- doc/.umirc.ts
+++ doc/.umirc.ts
@@ -3,8 +3,8 @@ import { defineConfig } from 'dumi';
 export default defineConfig({
   title: 'Stone Jin 解决方案',
   mode: 'site',
-  base: '/workflow/',
-  publicPath: '/workflow/',
+  base: '/',
+  publicPath: '/',
   locales: [['zh-CN', '中文']]
   // more config: https://d.umijs.org/config
 });
